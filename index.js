241be39c675a6679caf706d015b663aac99d3e9e
shanmukavenkat <21bq1a4208@vvit.net>
2024-08-21 15:53:46 +0000

    basic-understanding

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div>
+    <h1 className='greeting'>Hello World!</h1>
+    <p>Good to see you here.</p>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
